6a3e14333c6b6cc26d6acee38620eba277e25d25
dongdong <3460457627@qq.com>
2020-01-18 13:41:04 +0800

    update

--- Apps/Manager/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'Apps.Manager.apps.ManagerConfig'
--- Apps/Manager/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ManagerConfig(AppConfig):
-    name = 'Apps.Manager'
-    verbose_name = '后台管理'
-    verbose_name_plural = verbose_name
